4f777419585c891ee6cbf8361dee1e4278320aa3
Armandoedu <armandoeduardo306@gmail.com>
2024-08-07 18:46:38 -0300

    Codigo revisado

--- Jogo_Rua_Dispensario/Menu.py
+++ Jogo_Rua_Dispensario/Menu.py
@@ -1,4 +1,4 @@
-# from Game import Game
+
 import pygame
 import os
 from sys import exit
@@ -10,21 +10,13 @@ image_path =  sprites + '/menu/'
 
 
 class Menu:
-    def __init__(self, screen, screen_width, screen_height):
+    def __init__(self, screen_width, screen_height):
         pygame.init()
-        # self.width = 1472
-        # self.height = 704
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         pygame.display.set_caption("Rua do Dispensário")
         self.image_background = pygame.image.load(f'{image_path}dispeImagem.png')
-        self.background = (0,0,0)
         self.running = True
-        # self.title = pygame.font.Font(None, 72) 
-        # self.title_surface = self.title.render("Rua Do Dispensário", False, (28,28,28))
-        # self.title_rect = self.title_surface.get_rect(center=(screen_width/2, screen_height/2-200))
-        # self.instruction = pygame.font.Font(None, 50) 
-        # self.instruction_surface = self.instruction.render("Press ENTER", False, (28,28,28))
-        # self.instruction_rect = self.instruction_surface.get_rect(center=(screen_width/2, screen_height/2-50))
+
         
 
     def ruunning(self):    
@@ -37,17 +29,10 @@ class Menu:
             
                 if event.type == KEYDOWN:
                     if event.key == K_KP_ENTER or event.key == K_RETURN:
-                        print("Tecla enter pressionada")
                         self.running = False
-                        # pygame.quit()
-                        # exit()
             
             self.screen.blit(self.image_background, (0,0))
-            # self.screen.blit(self.title_surface, self.title_rect)
-            # self.screen.blit(self.instruction_surface, self.instruction_rect)
             pygame.display.flip()
 
         # pygame.quit()
-
-# teste = Menu()
-# teste.ruunning()
+        # exit()
