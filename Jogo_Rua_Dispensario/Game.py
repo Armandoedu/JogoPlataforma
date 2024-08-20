9bfa33da8de1fdc6a55c1deb7e1ff919c306dc42
Mallu <mmluluzinha044@gmail.com>
2024-08-20 14:52:09 -0300

    Cronometro

--- Jogo_Rua_Dispensario/Game.py
+++ Jogo_Rua_Dispensario/Game.py
@@ -7,6 +7,7 @@ from pygame.locals import *
 from sys import exit
 import pygame
 import os
+from Crono import Crono
 
 
 
@@ -20,7 +21,7 @@ musica_colisao = os.path.join(abs_path, 'Music', 'smw_1-up.wav')
 
 pygame.init()
 pygame.display.set_caption("Rua Do Dispensário")
-
+# font = pygame.font.SysFont("Arial", 30)
 
 screen_width = 900 
 screen_height = 600
@@ -38,6 +39,7 @@ class Game:
         self.musica = Sound(musica_fundo, musica_colisao)
         self.running = True
         self.state = "Menu"
+        self.crono = Crono(screen)
 
     def printGameOver(self):
         '''Imprime "Game Over! na tela."'''
@@ -55,12 +57,14 @@ class Game:
         self.game_map.draw()
         self.player.draw()
         self.obstacle.draw(screen)
+        self.crono.renderizar()
         pygame.display.flip()
 
     def update(self):
         self.player.handleKeys()
         self.player.update()
         self.obstacle.update()
+        self.crono.atualizar_tempo()
         if self.player.isCollision(self.obstacle):
             self.musica.playSound()
             self.printGameOver()
@@ -69,6 +73,24 @@ class Game:
         '''Toca a música de fundo do jogo.'''
         self.musica.playMusic()
         self.musica.setVolume(3)
+
+    # def renderizar(self):
+    #     self.mensagem = f"tempo: {self.x}"
+    #     self.textoformatado = font.render(self.mensagem, False, (255, 255, 255))
+    #     self.retTexto = self.textoformatado.get_rect()
+        
+    #     # Preenche a tela com uma cor antes de desenhar o texto (limpa a tela)
+    #     screen.fill((0, 0, 0))
+
+    #     # Centraliza o texto na screen
+    #     self.retTexto.center = (screen_width// 2, screen_height // 2)
+    #     screen.blit(self.textoformatado, self.retTexto)
+
+    #     # Atualiza a tela
+    #     pygame.display.flip()
+
+# Instancia o cronômetro
+# crono = Crono()
 
     def run(self):
         '''Roda o jogo.'''
@@ -85,6 +107,7 @@ class Game:
                         exit()
                 
                 self.draw(screen)
+                # self.renderizar()
                 self.update()
                 self.clock.tick(15)
         pygame.quit()
